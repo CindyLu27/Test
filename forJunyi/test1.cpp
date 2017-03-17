edf722dee871f538d5be04b3b38260359aecb236
CindyLu27 <cindy38381015@gmail.com>
2017-03-17 17:06:19 +0800

    ver 1.0.0

---
+++ forJunyi/test1.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  ProgramTest
+//
+//  Created by Cindy on 2017/3/15.
+//  Copyright © 2017年 Cindy Lu. All rights reserved.
+//
+
+#include <iostream>
+
+int outPutNum(int nNum)
+{
+    int nCount = 0;
+    for (int i = 1; i <= nNum; nNum++)
+    {
+        if(i%3 == 0 && i%5 ==0)
+        {
+            nCount ++;
+        }
+        else if (i%3 == 0)
+        {
+            continue;
+        }
+        else if(i%5 == 0)
+        {
+            continue;
+        }
+        else
+        {
+            nCount ++;
+        }
+    }
+    return nCount;
+}
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int nInt;
+    std::cout << "Please input a integer:" << std::endl;
+    std::cin >> nInt;
+    std::cout << outPutNum(nInt) << std::endl;
+    return 0;
+}
